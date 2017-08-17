66a82265ee5cdfa92055e4b81a1e98b60ac04d44
Roman <roman@romans-mbp.lan>
2017-08-17 17:26:36 -0400

    third commit

--- Routes/index.js
+++ Routes/index.js
@@ -1,20 +1,72 @@
-const express = require("express");
+const express = require("express"); //only need these two on this page
 const router  = express.Router();
+// const expressValidator = require('express-validator');
+// const bodyParser = require('body-parser'),
 
+let user = { username: "IronYard", password: "IronPints"};
 
-router.get("/", function (req, res) {
-  res.render("form");
+function authenticate(req, res, next) {
+  if (req.session.token) {
+    res.redirect("/results");
+  } else {
+    console.log("No token");
+    next();
+  }
+}
+
+router.get("/", authenticate, function(req, res) {
+  res.render("login"); // When you render it's a mustache, WE RENDER VIEWS
 });
 
-router.post("/submit" , function (req,res){
+router.get("/results", function(req, res, next) {
+  if (req.session.token) {
+    next();
+  } else {
+    res.redirect("/")
+  }
+}, function(req, res) {
+  res.render("results", req.session.user);
+});
 
-  req.checkBody("Email" , "Email cannot be empty.").notEmpty();
-  req.checkBody("username" , "Username must be at least 8 Characters Long.").islength(min:8);
-  req.checkBody("username" , "Username must be less than 25 Characters Long.").isLength({max:25});
-  // req.checkBody("Birthyear" , "Birthyear must be between 1900 and 2017.").isLength({min: 1900});
-  // req.checkBody("Birthyear" , "Birthyear must be between 1900 and 2017.").isLength({max: 2017});
-  req.checkBody("Password", "Password cannoy be empty").notEmpty({min: 8});
-  req.checkBody("Password" , "Password cannot be empty.").notEmpty();
+router.post("/results", function(req, res) {
+  let obj = {
+    username: req.body.username,
+    password: req.body.password
+  };
+
+  if (obj.username == user.username && obj.password == user.password) {
+    req.session.user = obj;
+    req.session.token = "afs29628";
+    res.redirect("/results");
+  } else {
+    res.redirect("/");
+  }
+});
+
+router.get("/logout", function(req, res) {
+  // req.session.destroy(); is good too
+  req.session.destroy(function(err) {
+    console.log(err);
+  });
+
+  res.redirect("/");
+});
+//VALIDATIONS========================================================================
+  router.post("/", function(req, res) {
+    req.checkBody("username" , "username cannot be empty.").notEmpty();
+    req.checkBody("username" , "Username must be at least 8 Characters Long.").isLength({min:8});
+    req.checkBody("username" , "Username must be at least 8 Characters Long.").alphaNummeric({min:8});
+
+    req.checkBody("username" , "Username must be less than 25 Characters Long.").isLength({max:25});
+    // req.checkBody("Birthyear" , "Birthyear must be between 1900 and 2017.").isLength({min: 1900});
+    // req.checkBody("Birthyear" , "Birthyear must be between 1900 and 2017.").isLength({max: 2017});
+    req.checkBody("Password", "Password cannot be empty").notEmpty({min: 8});
+    req.checkBody("Password" , "Password cannot be empty.").notEmpty();
+    req.check('Password', 'Error Message').notEmpty().isInt();
+
+
+  // app.use(bodyParser.bodyParser({ extended: true })); //ASK BRICE
+  // app.use(expressValidator([options]));  //ASK BRICE
 
   let errors = req.getValidationResult();
   let messages = [];
@@ -25,14 +77,12 @@ router.post("/submit" , function (req,res){
 
     let data = {
       errors: messages,
-      name: req.body.name,
-      email: req.body.email,
-      year: req.body.email,
-      position: req.body.position,
-      password: req.body.password,
+      username: req.body.name,
+      password: req.body.password
     };
 
     res.render('results', data);
     });
-    });
-    module.exports = r
+  });
+
+    module.exports = router;
